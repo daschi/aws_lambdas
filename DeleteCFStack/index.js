7a6682286943fbf5a45745147e398be22184108d
lindsey-s <ly.stevenson@gmail.com>
2019-12-09 13:06:38 -0800

    respond to delete event in DeleteCFStack lambda

--- DeleteCFStack/index.js
+++ DeleteCFStack/index.js
@@ -1,64 +1,75 @@
 const AWS = require('aws-sdk');
 const cf = new AWS.CloudFormation();
 
-async function handleWebhookEvent(webhookBody) {
-    const action = webhookBody['action'];
-    const pullRequest = webhookBody['pull_request'];
-    let message = 'Event ignored';
+function parsewebhookBody(eventType, webhookBody) {
+  let shouldDeleteStack, branchName, repoName;
 
-    // If no pull_request, ignore event
-    if(!pullRequest) {
-        return { statusCode: 200, message: message };
-    }
+  if (eventType === 'pull_request') {
+    branchName = webhookBody['pull_request']['head']['ref'];
+    repoName = webhookBody['pull_request']['head']['repo']['name'];
+    shouldDeleteStack = webhookBody['action'] === 'closed' && branchName != 'master'
+  } else if (eventType === 'delete') {
+    branchName = webhookBody['ref'];
+    repoName = webhookBody['repository']['name'];
+    shouldDeleteStack = webhookBody['ref_type'] === 'branch' && branchName !== 'master';
+  };
 
-    const branchName = pullRequest['head']['ref'];
-    const repoName = pullRequest['head']['repo']['name'];
-
-    console.log({action});
-    console.log({branchName});
-
-    if(action === 'closed' && branchName != 'master') {
-        const stackNameRepo = `${branchName}-${repoName}`;
-        const stackNameDev = `${stackNameRepo}-dev`;
-        const stackNameTest = `${stackNameRepo}-test`;
-        // Delete the stacks
-        await cf.deleteStack({StackName: stackNameRepo}).promise();
-        await cf.deleteStack({StackName: stackNameDev}).promise();
-        await cf.deleteStack({StackName: stackNameTest}).promise();
-        message = `CloudFormation Stacks deleted: ${stackNameRepo}, ${stackNameDev}, ${stackNameTest}`;
-    } else {
-        message = "Pull request event ignored";
-    }
-    return { statusCode: 200, body: message };
+  console.log('shouldDeleteStack', shouldDeleteStack);
+  const stackName = `${branchName}-${repoName}`
+  return { shouldDeleteStack, stackName }
 }
 
+async function handleWebhookEvent(eventType, webhookBody) {
+  if(!['pull_request', 'delete'].includes(eventType)) {
+    const message = `Event ignored. Event type: ${eventType}`;
+    return { statusCode: 200, message: message };
+  }
+
+  const { shouldDeleteStack, stackName } = parsewebhookBody(eventType, webhookBody);
+
+  if (!shouldDeleteStack) {
+    const message = `Event should not delete stack: shouldDeleteStack: ${shouldDeleteStack}, eventType: ${eventType}`;
+    return { statusCode: 200, message: message };
+  } else {
+    // Delete the stacks
+    const stackNameDev = `${stackName}-dev`;
+    const stackNameTest = `${stackName}-test`;
+    await cf.deleteStack({StackName: stackName}).promise();
+    await cf.deleteStack({StackName: stackNameDev}).promise();
+    await cf.deleteStack({StackName: stackNameDev}).promise();
+
+    const message = `CloudFormation Stacks deleted: ${stackName}, ${stackNameDev}, ${stackNameTest}`;
+    return { statusCode: 200, message: message };
+  }
+};
+
 module.exports.handler = async (event, context) => {
-    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-    let response;
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  let response;
 
-    if (!event.body) {
-        response = { statusCode: 403, body: 'Missing event body' };
-        console.log(`Response: ${JSON.stringify(response, null, 2)}`);
-        return response;
-    }
+  if (!event.body) {
+      response = { statusCode: 403, body: 'Missing event body' };
+      console.log(`Response: ${JSON.stringify(response, null, 2)}`);
+      return response;
+  }
 
+  let body;
+
+  try {
     // Parse the message body from the Lambda proxy
-    let body;
+    body = JSON.parse(event.body);
+  } catch (e) {
+    console.log(`Request Event Body is not JSON: ${event.body}`)
+    console.log(`Error: ${e}`)
+    response = { statusCode: 200, body: 'Event body is not JSON, Ignoring event' };
+    return response
+  }
 
-    try {
-      body = JSON.parse(event.body);
-    } catch (e) {
-      console.log(`Request Event Body is not JSON: ${event.body}`)
-      console.log(`Error: ${e}`)
-      response = { statusCode: 200, body: 'Event body is not JSON, Ignoring event' };
-      return response
-    }
+  console.log(`Event body: ${JSON.stringify(body, null, 2)}`);
 
-    console.log(`Event body: ${JSON.stringify(body, null, 2)}`);
+  const gitHubEvent = event.headers['X-GitHub-Event'];
+  response = await handleWebhookEvent(gitHubEvent, body);
 
-    // Handle the webhook event
-    response = await handleWebhookEvent(body);
-
-    console.log(`Response: ${JSON.stringify(response, null, 2)}`);
-    return response;
+  console.log(`Response: ${JSON.stringify(response, null, 2)}`);
+  return response;
 };
